a78c4040336f95b3053c7f55bbd79743d7b3713f
Josephtobi <josephtobi53@gmail.com>
2023-08-06 20:22:33 +0100

    restructure to live

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from typing import Optional
+from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
 
+
+@app.get("/whatsapp")
+def whatsapp_webhook(request: Request):
+
+    return int(request.query_params["hub.challenge"])
+
+
+@app.post("/whatsapp")
+async def whatsapp_process(request: Request):
+    note= await request.json()
+    print(note)
+
+    return Response('hello',status_code=200)
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+
