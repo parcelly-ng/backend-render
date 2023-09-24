0340dedb6d0bb03f3ff4fbfb728922552d1c1ce7
Josephtobi <josephtobi53@gmail.com>
2023-09-24 17:40:02 +0100

    Update main.py

--- session/main.py
+++ session/main.py
@@ -19,8 +19,8 @@ def check(number, url=url, headers=headers):
         "operation": "search_by_hash",
         "schema": "backend",
         "table": "session",
-        "hash_values": [number],
-        "get_attributes": ['session']
+        "hash_values": [number,],
+        "get_attributes": ['session',]
     }
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
